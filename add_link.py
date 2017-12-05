12b803982a5dfb4728d5d0868c28921c59548455
xychen <xychen91@126.com>
2017-12-05 17:10:44 +1100

    testset: real horse

--- add_link.py
+++ add_link.py
@@ -1,11 +1,12 @@
 file='fake_zebra.txt'
-model='ImagenetZebra_cycle_attn_X2'
-new_file='link_'+model+'_'+file
+#model='ImagenetZebra_cycle_attn_X2'
+model='real_horse'
+new_file='link_'+model+'.txt'
 fileWrite = open(new_file, 'w')
 fileRead=open(file)  
 line=fileRead.readline()
 while (''!=line):
-	new_line='https://raw.githubusercontent.com/napCC/PhotoRep/master/'+model+'_'+file[:-4]+'/'+line[:-5]+'_fake_B.png\n'
+	new_line='https://raw.githubusercontent.com/napCC/PhotoRep/master/'+model+'/'+line[:-5]+'.jpg\n'
 	fileWrite.write(new_line)
 	print('writing : '+line)
 	line=fileRead.readline()
