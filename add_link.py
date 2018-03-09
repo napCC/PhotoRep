4edfd570f355a5536c6dcd8cd8137bb72196903d
xychen <xychen91@126.com>
2018-03-09 17:10:44 +1100

    attentiongan_test200_results

--- add_link.py
+++ add_link.py
@@ -1,12 +1,12 @@
 file='fake_zebra.txt'
-#model='ImagenetZebra_cycle_attn_X2'
-model='real_horse'
+model='ImagenetZebra_cycle_attn_X2_test200_fake_zebra'
+#model='real_horse'
 new_file='link_'+model+'.txt'
 fileWrite = open(new_file, 'w')
 fileRead=open(file)  
 line=fileRead.readline()
 while (''!=line):
-	new_line='https://raw.githubusercontent.com/napCC/PhotoRep/master/'+model+'/'+line[:-5]+'.jpg\n'
+	new_line='https://raw.githubusercontent.com/napCC/PhotoRep/master/'+model+'/'+line[:-5]+'.png\n'
 	fileWrite.write(new_line)
 	print('writing : '+line)
 	line=fileRead.readline()
