a20ec67164d47e2d83fa6b76cfa7907f7b73dba5
xychen <xychen91@126.com>
2018-03-10 18:42:59 +1100

    new real tiger:good size

--- add_link.py
+++ add_link.py
@@ -1,12 +1,12 @@
 file='real_tiger.txt'
 #model='ImagenetZebra_cycle_attn_X1_test160_fake_horse'
-model='CycleGAN_fake_leopard'
+model='real_tiger'
 new_file='link_'+model+'.txt'
 fileWrite = open(new_file, 'w')
 fileRead=open(file)  
 line=fileRead.readline()
 while (''!=line):
-	new_line='https://raw.githubusercontent.com/napCC/PhotoRep/master/'+model+'/'+line[:-6]+'_fake_B.png\n'
+	new_line='https://raw.githubusercontent.com/napCC/PhotoRep/master/'+model+'/'+line[:-6]+'_real_A.png\n'
 	fileWrite.write(new_line)
 	print('writing : '+new_line)
 	line=fileRead.readline()
