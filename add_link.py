971944eee77d2d69da94995644b9c6f47bd20b5b
xychen <xychen91@126.com>
2018-03-10 18:11:58 +1100

    add fake leopard

--- add_link.py
+++ add_link.py
@@ -1,12 +1,12 @@
-file='fake_zebra.txt'
+file='real_tiger.txt'
 #model='ImagenetZebra_cycle_attn_X1_test160_fake_horse'
-model='CycleGAN_fake_horse'
+model='CycleGAN_fake_leopard'
 new_file='link_'+model+'.txt'
 fileWrite = open(new_file, 'w')
 fileRead=open(file)  
 line=fileRead.readline()
 while (''!=line):
-	new_line='https://raw.githubusercontent.com/napCC/PhotoRep/master/'+model+'/'+line[:-5]+'.png\n'
+	new_line='https://raw.githubusercontent.com/napCC/PhotoRep/master/'+model+'/'+line[:-6]+'_fake_B.png\n'
 	fileWrite.write(new_line)
 	print('writing : '+new_line)
 	line=fileRead.readline()
