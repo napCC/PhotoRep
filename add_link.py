ff3e7688439d76b0d8e95fc919a50dfa2fd21510
xychen <xychen91@126.com>
2018-03-10 22:08:36 +1100

    upload fake orange

--- add_link.py
+++ add_link.py
@@ -1,12 +1,12 @@
-file='real_tiger.txt'
-#model='ImagenetZebra_cycle_attn_X1_test160_fake_horse'
-model='real_leopard'
-new_file='link_'+model+'.txt'
+file='real_apple/list.txt'
+#model='ImagenetTiger_cycle_attn_X3_fake_tiger'
+model='Apple2Orange_cycle_attn_gan_2_fake_orange'
+new_file=model+'/'+'link_list'+'.txt'
 fileWrite = open(new_file, 'w')
 fileRead=open(file)  
 line=fileRead.readline()
 while (''!=line):
-	new_line='https://raw.githubusercontent.com/napCC/PhotoRep/master/'+model+'/'+line[:-6]+'_real_B.png\n'
+	new_line='https://raw.githubusercontent.com/napCC/PhotoRep/master/'+model+'/'+line[:-5]+'_fake_B.png\n'
 	fileWrite.write(new_line)
 	print('writing : '+new_line)
 	line=fileRead.readline()
