dd0a3fd73694b94be8d486f7077636edb8c1d261
napCC <11401957+napCC@users.noreply.github.com>
2019-12-07 00:33:41 +1100

    gup

--- horse2zebra_coco/background_comparison.py
+++ horse2zebra_coco/background_comparison.py
@@ -4,16 +4,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 from pdb import set_trace as st
-
-horse2zebra=True
-AttnGANFold='/home/xche6658/code/pytorch-CycleGAN-and-pix2pix/results/horse2zebra_coco/zebra_attnganX2_featloss_sparse2/test_200/images/'
+horse2zebra=False
+AttnGANFold='/home/xche6658/code/pytorch-CycleGAN-and-pix2pix/results/horse2zebra_coco/zebra_attnganX2-200_featloss_sparse1/test_200/images/'
 if horse2zebra:
   suffix='_real_A_fake_B.png'
+#  suffix='_fake_B.png'
   CycleGANFold='CycleGAN_fake_zebra/'
   segFold='segmentation_horse/'
   SourceFold='real_horse/'
 else:# zebra2horse
   suffix='_real_A_fake_A.png'
+ # suffix='_fake_A.png'
   CycleGANFold='CycleGAN_fake_horse/'
   segFold='segmentation_zebra/'
   SourceFold='real_zebra/'
@@ -39,7 +40,6 @@ while (''!=fileCycle):
     ImageCycle = cv2.imread(CycleGANFold + fileCycle[:-1])
     ImageSource = cv2.imread(SourceFold + fileSource[:-1])
     ImageAttnGAN= cv2.imread(AttnGANFold+fileSource[:12]+suffix)
-    print(fileseg[:-1])
     
     ImageSeg = 255 - cv2.imread(segFold+fileseg[:-1])
     err_psnr_cylce = psnr(ImageSource*ImageSeg, ImageCycle*ImageSeg)
